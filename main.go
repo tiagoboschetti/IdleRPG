dc4c592cfa5c3086233d736de23a1b29f622cf22
Tiago Daniel Boschetti <tiuuhs@gmail.com>
2017-12-08 23:05:12 -0200

    Correçao da organizaçao das structs Classes (arqueiro, guerreiro, mago) para o main, pois estando no outro arquivo o main nao conseguia importar. Go, organiza os arquivos em pacotes entao os imports de outros aquivos sao reservados para metodos reutilizaveis.

--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import "fmt"
 
+type Classe struct {
+	vida   int
+	defesa int
+	ataque int
+}
+
+func (c *Classe) Vida() int {
+	return c.vida
+}
+
+func (c *Classe) Defesa() int {
+	return c.defesa
+}
+
+func (c *Classe) Ataque() int {
+	return c.ataque
+}
+
+type Arqueiro struct {
+	Classe
+}
+
+type Guerreiro struct {
+	Classe
+}
+
+type Mago struct {
+	Classe
+}
+
+func main() {
+	a := Arqueiro{Classe: Classe{vida: 10, defesa: 3, ataque: 5}}
+	fmt.Println("Arqueiro")
+	fmt.Println("  > Vida: ", a.vida)
+	fmt.Println("  > Defesa: ", a.defesa)
+	fmt.Println("  > Ataque: ", a.ataque)
+
+	g := Guerreiro{Classe: Classe{vida: 15, defesa: 6, ataque: 2}}
+	fmt.Println("Guerreiro")
+	fmt.Println("  > Vida: ", g.vida)
+	fmt.Println("  > Defesa: ", g.defesa)
+	fmt.Println("  > Ataque: ", g.ataque)
+
+	m := Mago{Classe: Classe{vida: 8, defesa: 2, ataque: 8}}
+	fmt.Println("Mago")
+	fmt.Println("  > Vida: ", m.vida)
+	fmt.Println("  > Defesa: ", m.defesa)
+	fmt.Println("  > Ataque: ", m.ataque)
 }
--- src/classes.go
+++ src/classes.go
@@ -1,67 +1,16 @@
 package classes
 
-type Classe interface {
-	vida() int
-	defesa() int
-	ataque() int
-}
-
-type Arqueiro struct {
-}
-
-func (a *Arqueiro) Vida() int {
-	return 10
-}
-
-func (a *Arqueiro) Defesa() int {
-	return 4
-}
-
-func (a *Arqueiro) Ataque() int {
-	return 5
-}
-
-func (a Arqueiro) Desviar() bool {
+func (a *Arqueiro) Desviar() bool {
 	// toda vez que receber ataque terá uma chance de desviar do ataque
 	return true
 }
 
-type Guerreiro struct {
-}
-
-func (a *Guerreiro) Vida() int {
-	return 13
-}
-
-func (a *Guerreiro) Defesa() int {
-	return 6
-}
-
-func (a *Guerreiro) Ataque() int {
-	return 3
-}
-
-func (g Guerreiro) Defender() bool {
+func (g *Guerreiro) Defender() bool {
 	// chance de ADO
 	return true
 }
 
-type Mago struct {
-}
-
-func (a *Mago) Vida() int {
-	return 8
-}
-
-func (a *Mago) Defesa() int {
-	return 2
-}
-
-func (a *Mago) Ataque() int {
-	return 8
-}
-
-func (m Mago) Castar() bool {
+func (m *Mago) Castar() bool {
 	// Tem chance de causar double damage
 	return true
 }
