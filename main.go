3cf137d0cf537f5943a29a4dc5635d419e0e505d
Tiago Daniel Boschetti <tiuuhs@gmail.com>
2017-12-13 22:45:32 -0200

    Adicionado polimorfismo na Classe.

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type ClasseProvider interface {
+	GetClasse() *Classe
+	Desviar() bool
+	Defender() bool
+	Castar() bool
+}
+
+func (c *Classe) GetClasse() *Classe {
+	return c
+}
 
 type Classe struct {
 	vida   int
@@ -20,6 +34,29 @@ func (c *Classe) Ataque() int {
 	return c.ataque
 }
 
+func Atacar(ppAtacante, ppDefensor ClasseProvider) *Classe {
+
+	if reflect.TypeOf(ppAtacante).Name() == "Mago" && ppAtacante.Castar() {
+		// procou o double damage
+		aAtaque := ppAtacante.GetClasse().ataque
+		aDanoCausadoAoDefensor := aAtaque * 2
+		fmt.Println(">>>>DOUBLE DAMAGE<<<<")
+	}
+
+	if reflect.TypeOf(ppDefensor).Name() == "Arqueiro" && ppDefensor.Desviar() {
+		// procou o evasion
+		aDanoCausadoAoDefensor := 0
+		fmt.Println(">>>>EVASION<<<<")
+	}
+
+	if reflect.TypeOf(ppDefensor).Name() == "Guerreiro" && ppDefensor.Defender() {
+		// procou o ADO
+		aDanoCausadoAoDefensor := ppAtacante.GetClasse().ataque
+		aDanoCausadoAoAtacante := ppDefensor.GetClasse().ataque
+		fmt.Println(">>>>ATAQUE DE OPORTUNIDADE<<<<")
+	}
+}
+
 type Arqueiro struct {
 	Classe
 }
@@ -30,6 +67,25 @@ type Guerreiro struct {
 
 type Mago struct {
 	Classe
+}
+
+func (a *Arqueiro) Desviar() bool {
+	// toda vez que receber ataque terá uma chance de desviar do ataque
+	// RETORNA TRUE CASO O EVADE TENHA SIDO BEM SUCEDIDO
+	return true
+
+}
+
+func (g *Guerreiro) Defender() bool {
+	// chance de ADO
+	// RETORNA TRUE CASO O ATAQUE DE OPORTUNIDADE TENHA PROCADO
+	return true
+}
+
+func (m *Mago) Castar() bool {
+	// Tem chance de causar double damage
+	// RETORNAR TRUE CASO PROQUE O DOUBLE DAMAGE
+	return true
 }
 
 func main() {
